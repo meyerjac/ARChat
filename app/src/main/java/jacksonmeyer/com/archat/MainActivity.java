a48cdb6298478a86cd0a325ebb98fd2793458143
jackson meyer <jackson.meyer7@gmail.com>
2017-10-09 10:29:54 -0700

    added dependencies, Butterknife, and setupLogin

--- app/src/main/java/jacksonmeyer/com/archat/MainActivity.java
+++ app/src/main/java/jacksonmeyer/com/archat/MainActivity.java
@@ -1,13 +1,43 @@
 package jacksonmeyer.com.archat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.nameTextField)
+    EditText mNameTextField;
+    @Bind(R.id.emailTextField)
+    EditText mEmailTextField;
+    @Bind(R.id.passwordTextField)
+    EditText mPasswordTextField;
+    @Bind(R.id.registerUserButton)
+    Button mRegisterUserButton;
+    @Bind(R.id.loginButton)
+    Button mLoginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mLoginButton.setOnClickListener(this);
+        mRegisterUserButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterUserButton) {
+            Log.d("Main Activity", "register");
+        } else if (view == mLoginButton){
+            Log.d("Main Activity", "login");
+        }
     }
 }
